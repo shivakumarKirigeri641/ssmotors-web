09f85ad443bea09f09867771660ccec090ec726f
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-05-27 15:57:47 +0530

    fix:text included for logout icon

--- src/components/customComponents/SSMotorsNavBar.js
+++ src/components/customComponents/SSMotorsNavBar.js
@@ -109,6 +109,7 @@ const SSMotorsNavBar = () => {
                 <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
                 <circle cx="12" cy="7" r="4" />
               </svg>
+              <p className="text-sm font-semibold">Logout</p>
             </div>
           )}
         </Link>
