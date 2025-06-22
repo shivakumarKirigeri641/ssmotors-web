3c6a4547dfa7b9351bc14200e9386abf54290928
Shivakumar Kirigeri <shivakumar641@gmail.com>
2025-06-22 14:16:16 +0530

    task: on login working, after logging in, need to check

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,18 +3,21 @@ import { SERVER } from "../utils/constants";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addAdmin } from "../store/slices/adminSlice";
+import { useNavigate } from "react-router";
 const Login = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [email, setloginemail] = useState("shiva@gmail.com");
   const [password, setloginpassword] = useState("Shiva@123");
   const handleLogin = async () => {
-    const result = await axios(
+    const result = await axios.post(
       SERVER + "/admin/login",
       { email, password },
       { withCredentials: true }
     );
     console.log(result);
     dispatch(addAdmin(result?.data?.data));
+    navigate("/dashboard");
   };
   return (
     <div>
--- src/components/dashboardcomponents/Dashboard.js
+++ src/components/dashboardcomponents/Dashboard.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
+  const admin = useSelector((store) => store.admin);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!admin) {
+      navigate("/");
+    }
+  });
   return (
     <div>
-      <p>Dashboard</p>
+      <div
+        className="h-screen large uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light"
+        data-src={require("../../images/wp2575889.webp")}
+        uk-img="loading: eager"
+      >
+        <h1></h1>
+      </div>
     </div>
   );
 };
